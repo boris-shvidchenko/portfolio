0e495b8c2531f810e943a9187e233af6aa6396a8
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-10-27 15:01:46 -0700

    Styles added to Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,25 @@
 // Components
 import Link from 'next/link';
 
+// Heroicons
+import { SunIcon } from '@heroicons/react/24/outline';
+import { XMarkIcon } from '@heroicons/react/24/solid';
+import { Bars3Icon } from '@heroicons/react/24/solid';
+
+
 export default function Navbar() {
     return(
-        <nav className='flex justify-between mx-20 mt-7 items-center'>
-            <h1 className='ml-3 text-3xl'>Boris Shvidchenko</h1>
-            <div className='flex space-x-10'>
-                <Link href='/about'>About</Link>
-                <Link href='/projects'>Projects</Link>
-                <Link href='/contact'>Contact</Link>
-            </div>
+            <nav className='flex justify-between px-20 py-9 items-center sticky top-0 bg-white z-100 border-b border-b-stone-500 md:border-none'>
+                <h1 className='text-3xl'>Boris Shvidchenko</h1>
+                <div className='hidden space-x-10 mt-1 items-center md:flex'>
+                    <Link href='/about'><p className='text-lg lg:text-xl'>About</p></Link>
+                    <Link href='/projects'><p className='text-lg lg:text-xl'>Projects</p></Link>
+                    <Link href='/contact'><p className='text-lg lg:text-xl'>Contact</p></Link>
+                    <SunIcon className='icons'/>
+                </div>
+                <Bars3Icon className='icons mt-1 md:hidden'/>
 
-        </nav>
+            </nav>
     )
 }
+
