f5686c3978bf443f677d7b77f361e529b23e44e9
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-02 11:05:12 -0800

    update

--- components/Bio.js
+++ components/Bio.js
@@ -24,10 +24,10 @@ export default function Bio() {
             <section className='md:flex md:space-x-6 lg:space-x-10 md:mt-8'>
                 <img src='../images/profile-picture.png' alt='Profile picture' className={`w-screen md:w-80 h-full lg:w-[450px] md:mt-2 md:rounded-sm`} />
 
-                <div className={`bg-gradient-to-b from-black/70 md:bg-none text-white ${textColor} pt-[92px] sm:pt-[120px] md:pt-0 pb-8 md:pb-0 absolute top-0 px-7 md:px-none md:relative md:top-0`}>
+                <div className={`bg-gradient-to-b from-black/70 md:bg-none text-white ${textColor} pt-[92px] sm:pt-[120px] md:pt-0 pb-8 md:pb-0 absolute top-0 px-7 md:px-none md:relative md:top-0 `}>
                     
-                    <h1 className='text-2xl mb-3 lg:mb-5 font-semibold'>About Me</h1>
-                    <div className={`${mobileView.width <= 470 ? 'text-md' : ''}`}>
+                    <h1 className={`text-2xl mb-3 lg:mb-5 font-semibold ${mobileView.width <= 470 ? 'text-xl -mt-1' : ''}`}>About Me</h1>
+                    <div className={`${mobileView.width <= 470 ? 'text-sm -mt-2' : ''}`}>
                         <p className='mb-2 lg:mb-4'>I am a front end developer with a background in geospatial data analysis and GIS. My interests include web development and building functional applications for a variety of uses. I enjoy the process of designing clean and effective websites, from the initial planning and design phases all the way to the problem solving and debugging, and the creativity and logic that is required in order to successfully deploy a project. The more complex the build, the better!</p>
                         <p className='mb-2 lg:mb-4'>Take a look at some of my personal projects and feel free to contact me at any time.</p>
                         <p>When not working you can find me paragliding, trail running, traveling, and training for future goals.</p>
