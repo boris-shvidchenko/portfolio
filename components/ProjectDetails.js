1fa1f8274449f7feda78c5f4f0e283a4655b2302
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-19 14:49:29 -0800

    Added new project data

--- components/ProjectDetails.js
+++ components/ProjectDetails.js
@@ -24,13 +24,22 @@ export default function ProjectDetails({ data }) {
                 <p className='text-2xl mt-5 md:mt-0 mb-3 lg:mb-5 font-semibold'>{data?.title}</p>
                 <p className='mb-2 lg:mb-4'>{data?.description}</p>
                 <p className='mb-2 lg:mb-4'>{data?.description2}</p>
-                <Link 
-                    href={data?.url} 
-                    target='_blank' 
-                    className={`w-28 mt-7 sm:left-0 sm:static flex space-x-2 rounded-md md:border ${borderMedium} p-2 ${bgColor} ${bgColorMedium} min-w-fit onhover-social onhover`}
-                    >
-                    <p>Check out the build</p>
-                </Link>
+                <section className='flex space-x-5'>
+                    <Link 
+                        href={data?.url} 
+                        target='_blank' 
+                        className={`w-28 mt-7 sm:left-0 sm:static flex space-x-2 rounded-md md:border ${borderMedium} p-2 ${bgColor} ${bgColorMedium} min-w-fit onhover-social onhover`}
+                        >
+                        <p>Check out the build</p>
+                    </Link>
+                    <Link 
+                        href={data?.url2} 
+                        target='_blank' 
+                        className={`w-28 mt-7 sm:left-0 sm:static flex space-x-2 rounded-md md:border ${borderMedium} p-2 ${bgColor} ${bgColorMedium} min-w-fit onhover-social onhover`}
+                        >
+                        <p>Source Code</p>
+                    </Link>
+                </section>
             </section>
         </main>
     )
--- projectData.js
+++ projectData.js
@@ -1,14 +1,15 @@
 // Array will store individual project information for the projects page 
 export const projectData = [
     {
-        id: 'trivia',
+        id: 'flashcards',
         key: '1',
-        title: 'Trivia!',
-        summary: 'A responsive trivia game.',
-        description: "This was one of the first projects that I built using React with the Next.js framework, where I wasn't following tutorials or courses to complete the work. This was built entirely from scratch and was a fun and rewarding learning experience. Furthermore, this game was built using Tailwind CSS 3.0." ,
-        description2: "The primary goal for this project was to gain an understanding of basic API usage and data manipulation in a more complex development environment, whilst trying to keep UI/UX design principals in mind to ensure a clean and responsive build. The data was obtained from the Open Trivia Database, which offers a free to use database with an extensive selection of questions. Although I am far from being an expert web designer I had a great time experimenting with various design principals/techniques and I hope to apply, and improve on, what I've learned from this project to my other builds. Take a look and let me know what you think!",
-        img: 'images/project-images/trivia.png',
-        url: 'https://trivia-bshvid.vercel.app/'
+        title: 'Flashcard App',
+        summary: 'A useful flashcard app.',
+        description: 'Eventually branching out towards backend development has been a goal of mine ever since I started learning front end, both due to an interest in it and in order to become a better and more well rounded developer, and this project was a way for me to get some hands on experience with that. This application allows users to create flashcards and, by logging in with their Gmail accounts, save their collections for future reference. Editing capabilities have also been implemented, alongside the ability to delete cards, sort saved cards, and shuffle collections.',
+        description2: 'This was an enjoyable build to work on which allowed me to get comfortable with using Firebase to store card data, authenticate users, and explore interesting dependencies such as React Firebase Hooks and the React Loader Spinner (for asynchronous operations). Debugging and trying to piece together an app that had quite a few moving parts, including a decent amount of state to manage, was also rewarding. Let me know your thoughts, especially if there is anything that I could improve on. The icons that I used came from Heroicons and Icons8.',
+        img: 'images/project-images/flashcards.png',
+        url: 'https://flashcards-bshvid.vercel.app/',
+        url2: 'https://github.com/boris-shvidchenko/flashcard-app'
     },
     {
         id: 'cafe',
@@ -18,26 +19,29 @@ export const projectData = [
         description: "The main objective for this project was to focus on creating a clean and effective website that followed the fundamentals of web design, including: the use of white space, visual hierarchy, fonts, and color contrast. The idea was to create a fictional coffee shop/café website that was simple in design yet responsive and user friendly no matter what device the website was viewed on. This was an enjoyable build that allowed me to experiment with various looks to see what worked (and what didn't). ",
         description2: 'The majority of images that I used came from Pexels and the icons were either from Heroicons or from Icons8.    ',
         img: 'images/project-images/cafe.png',
-        url: 'https://cafe-bshvid.vercel.app/'
+        url: 'https://cafe-bshvid.vercel.app/',
+        url2: 'https://github.com/boris-shvidchenko/cafe-website'
     },
     {
-        id: 'project-3',
+        id: 'trivia',
         key: '3',
-        title: 'placeholder',
-        summary: 'placeholder',
-        description: 'placeholder',
-        description2: '',
-        img: '#',
-        url: '#'
+        title: 'Trivia!',
+        summary: 'A responsive trivia game.',
+        description: "This was one of the first projects that I built using React with the Next.js framework, where I wasn't following tutorials or courses to complete the work. This was built entirely from scratch and was a fun and rewarding learning experience. Furthermore, this game was built using Tailwind CSS 3.0." ,
+        description2: "The primary goal for this project was to gain an understanding of basic API usage and data manipulation in a more complex development environment, whilst trying to keep UI/UX design principals in mind to ensure a clean and responsive build. The data was obtained from the Open Trivia Database, which offers a free to use database with an extensive selection of questions. Although I am far from being an expert web designer I had a great time experimenting with various design principals/techniques and I hope to apply, and improve on, what I've learned from this project to my other builds. Take a look and let me know what you think!",
+        img: 'images/project-images/trivia.png',
+        url: 'https://trivia-bshvid.vercel.app/',
+        url2: 'https://github.com/boris-shvidchenko/trivia-game'
     },
     {
         id: 'project-4',
-        key: '4',
+        key: '5',
         title: 'placeholder',
         summary: 'placeholder',
         description: 'placeholder',
         description2: '',
         img: '#',
-        url: '#'
+        url: '#',
+        url2: ''
     }
 ]
