40e5295d6649d362f724cd01fd4751d50be6659b
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-29 21:40:10 -0800

    Updated UI for loading comp.

--- components/Loading.js
+++ components/Loading.js
@@ -1,10 +1,17 @@
 // Hooks
 import { usePromiseTracker } from 'react-promise-tracker';
+import { useContext } from 'react';
+
+// Context
+import { Context } from '../pages/_app';
 
 // Spinner
 import { TailSpin } from 'react-loader-spinner';
 
 export default function Loading() {
+
+    // Access the application states
+    const { darkTheme } = useContext(Context);
 
     // Boolean value where true = loading, false = not loading
     const { promiseInProgress } = usePromiseTracker(); 
@@ -14,7 +21,7 @@ export default function Loading() {
         <TailSpin
             height="25"
             width="25"
-            color="#000000"
+            color={`${darkTheme ? '#ffffff' : '#000000'}`}
             ariaLabel="tail-spin-loading"
             radius="1"
             wrapperStyle={{}}
